ddb22efb60f7d2b0aad7d8cad7a0fd031013fec0
mitsuhiro0602 <0hornet0@gmail.com>
2019-12-03 18:43:52 +0900

    create-message

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
+    resources :posts, only: [:index, :create]
   end
 end
